06177d9b4f0dc3ef55a8f9340bc834aaf42638df
Chandanku123 <chandanpandeyprog@gmail.com>
2022-09-01 22:44:01 +0530

    ReverseNumber

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] nums ={2,3,4,2,3};
+        
+            var result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var num = Math.Abs(nums[i]);
+                if (nums[num - 1] < 0)
+                    result.Add(num);
+                else
+                    nums[num - 1] *= -1;
+            }
+            System.Console.WriteLine(result);
+        }
+}
